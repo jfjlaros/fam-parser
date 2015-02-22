5203b8a230e125b4b98c7b842e5b5c093cbf8bd2
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2015-02-22 17:18:18 +0100

    Documentation.

--- fam_parser/container.py
+++ fam_parser/container.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 
 """
+Container that acts like an ordered default dictionary.
 """
 
 import collections
 
 class Container(collections.OrderedDict):
+    """
+    Simple ordered default dictionary.
+    """
     def __getitem__(self, key):
         if key not in self.keys():
             self[key] = 0;
