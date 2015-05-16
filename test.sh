c14481004c9c4189db885872a03cc068301ba03c
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2015-05-16 15:03:27 +0200

    More extensive testing.

--- test.sh
+++ test.sh
@@ -4,11 +4,14 @@ TEMP_FILE="/tmp/.test_$RANDOM"
 
 echo -n "Testing Python interface "
 for i in data/*.fam data/*.FAM; do
-  python -m python.fam_parser $i $TEMP_FILE
+  python -m python.fam_parser -d -e $i $TEMP_FILE
   
-  if ! grep -a "EOF_MARKER: End of File" $TEMP_FILE > /dev/null; then
+  if ! grep "^EOF_MARKER: End of File$" $TEMP_FILE > /dev/null; then
     echo
     echo Test failed for file $i.
+  elif grep "^_RAW_[0-9][0-9]:.* 0d" $TEMP_FILE > /dev/null; then
+    echo
+    echo Possible unhandled text field in file $i.
   else
     echo -n .
   fi
@@ -19,7 +22,7 @@ echo -n "Testing JavaScript interface "
 for i in data/*.fam data/*.FAM; do
   nodejs javascript/cli.js $i > $TEMP_FILE
   
-  if ! grep -a "EOF_MARKER: 'End of File'" $TEMP_FILE > /dev/null; then
+  if ! grep "^  EOF_MARKER: 'End of File' }$" $TEMP_FILE > /dev/null; then
     echo
     echo Test failed for file $i.
   else
